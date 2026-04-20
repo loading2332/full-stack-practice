31d24c10d42a3ca41d3d3054a664c0e8bc4b5a72
hugoxin_i <hugoxin_i@didiglobal.com>
2026-04-20 18:05:22 +0800

    feat:完善入口文件

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  const allowedOrigins = process.env.CORS_ORIGIN
+    ? process.env.CORS_ORIGIN.split(',')
+    : ['http://localhost:3000'];
+
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  await app.listen(3001);
 }
 bootstrap();
